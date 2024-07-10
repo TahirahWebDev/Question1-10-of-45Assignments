e1abd8fd24619b176ee21272710facb6104248ea
Tahirah Roohi <157677736+TahirahWebDev@users.noreply.github.com>
2024-07-10 01:12:29 -0700

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,61 @@
+// Question 2
+
+// let anyName: string = "Tahirah";
+// let message: string = `Hello ${anyName}, Would you like to learn some Python today?`;
+// console.log(message);
+
+
+// Question 3
+
+// let personName: string = "Tahirah Roohi";
+// let lowerCaseName: string = personName.toLowerCase();
+// let upperCaseName: string = personName.toUpperCase();
+// let titleCaseName: string = personName.replace(/\b\w/g, char => char.toUpperCase())
+
+// console.log("Lowercase:", lowerCaseName);
+// console.log("Uppercase:", upperCaseName);
+// console.log("Titlecase:", titleCaseName);
+
+
+// Question 4
+
+// let quote: string = "A person who never made a mistake never tried anything new.";
+// let author: string = "Albert Einstein";
+// console.log(`${author} once said, "${quote}"`);
+
+
+// Question 5
+
+// const famous_person: string = "Albert Einstein";
+// const quote: string = "A person who never made a mistake never tried anything new.";
+// const message: string = `${famous_person} once said, "${quote}"`;
+// console.log(message);
+
+
+// Question 6
+
+// const nameWithspace: string = "\t\n   John Doe   \n\t";
+// console.log("Name with whitespace:", nameWithspace);
+// const strippedName: string = nameWithspace.trim();
+// console.log("Stripped name:", strippedName);
+
+
+// Question 7 & 8
+
+// console.log(5 + 3);
+// console.log(10 - 2);
+// console.log(4 * 2);
+// console.log(16 / 2);
+
+
+// Question 9
+
+// const favoriteNumber: number = 4;
+// const message: string = `My favorite number is ${favoriteNumber}.`;
+// console.log(message);
+
+
+// Question 10
+
+// Aready Done
+// let myName: string = "Tahirah Roohi"
